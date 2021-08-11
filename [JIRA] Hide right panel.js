34666349c859d2a8ad252658c83532942755bb02
aurelien-CAPI <73163010+aurelien-CAPI@users.noreply.github.com>
2021-08-11 17:13:44 +0200

    Create [JIRA] Hide right panel.js

---
+++ [JIRA] Hide right panel.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name         JIRA CONFIG RIGHT PANEL
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Hide or show right panel in jira
+// @author       Aurélien CAPI
+// @match        https://jira.adeo.com/browse/ISO-*
+// @icon         https://www.google.com/s2/favicons?domain=adeo.com
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    unsafeWindow.hideRightPanel = function() {
+        document.getElementById('viewissuesidebar').style.display = 'none';
+        var html = '<a class="aui-button toolbar-trigger issueaction-comment-issue inline-comment" href="javascript:void(showRightPanel());" resolved=""><span class="icon aui-icon aui-icon-small aui-iconfont-watch-filled"></span> <span class="trigger-label">Show right panel</span></a>'
+        document.getElementById('opsbar-opsbar-admin').innerHTML = html;
+    }
+
+    unsafeWindow.showRightPanel = function() {
+        document.getElementById('viewissuesidebar').style.display = 'table-cell';
+        var html = '<a class="aui-button toolbar-trigger issueaction-comment-issue inline-comment" href="javascript:void(hideRightPanel());" resolved=""><span class="icon aui-icon aui-icon-small aui-iconfont-watch-filled"></span> <span class="trigger-label">Hide right panel</span></a>'
+        document.getElementById('opsbar-opsbar-admin').innerHTML = html;
+    }
+
+
+    unsafeWindow.showRightPanel();
+
+})();
+
